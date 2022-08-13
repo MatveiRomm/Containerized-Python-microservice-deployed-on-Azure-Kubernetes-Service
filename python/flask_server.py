3667b914992bc75b2fb50bc3a9255c253498e631
Matvei Romm <72136238+MatveiRomm@users.noreply.github.com>
2022-08-13 13:26:26 +0200

    Update flask_server.py

--- python/flask_server.py
+++ python/flask_server.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 import bitcoin_output
+from os import environ
 
 app = Flask(__name__)
 
-@app.route("/")
+password = environ['MY_PASS']
+
+@app.route("/", methods=["GET"])
 def summary():
-    return jsonify(bitcoin_output.showJson())
+    headers = request.headers
+    auth = headers.get("x-Api-Key")
+    if auth == password:
+        return jsonify(bitcoin_output.showJson())
+    else:
+        abort(401)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
+    app.run()
